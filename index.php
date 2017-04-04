a64b6606d0cfe5e933cd33916c136d124118ff9a
Nefrick <it.nefrick@gmail.com>
2017-04-04 15:01:09 +0300

    Change js

--- index.php
+++ index.php
@@ -8,9 +8,7 @@
   <link rel="stylesheet" href="css/app.css">
   <link rel="stylesheet" href="style.css">
   <script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>
+      src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 
   <title>Document</title>
 
